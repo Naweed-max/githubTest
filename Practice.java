d0188240a35c0bcf51ebd55b48b533f0fa9f543e
Naweed-max <naweed_3aem@outlook.com>
2024-09-08 16:03:55 +0300

    addition class

---
+++ Practice.java
@@ -0,0 +1,11 @@
+class AddTowIntegerNumbers {
+    public static void main(String[] args) {
+
+        int firstnumber = 10, secondnumber = 21, result;
+        result = firstnumber + secondnumber;
+        System.out.print("The result is:" + result);
+
+        // new notes
+
+    }
+}
